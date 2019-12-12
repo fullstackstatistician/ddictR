8321d56edb21689d29ff29cd5cf8805bde58f01d
Omair A. Khan <omair.adil.khan@gmail.com>
2019-12-12 11:00:22 -0600

    updated create_individual_entry.R with all changes requested in https://github.com/fullstackstatistician/ddictR/issues/2 except running chapter header.

--- R/create_individual_entry.R
+++ R/create_individual_entry.R
@@ -1,38 +1,51 @@
 create_individual_entry <- function(data = merged.df, metadata = metadata.df, variable) {
-  write_out(paste0('## ', variable, '\n'))
+  write_out(paste0('## Variable: ', variable, '\n'))
   
   if (!is.na(metadata[metadata[["variable"]] == variable, "label"])) {
-    write_out(paste0('#### ', metadata[metadata[["variable"]] == variable, "label"], "\n"))
+    write_out(paste0('#### Label: ', metadata[metadata[["variable"]] == variable, "label"], "\n"))
   }
   
   write_out("\n")
   
-  chunk_wrapper(
-    plot_distribution(variable = variable), 
-    options = c("echo = FALSE", "warning = FALSE", "message = FALSE", "fig.height = 2.5", "fig.width = 4.2", "fig.align = 'center'")
-  )
-  
-  write_out("\n")
-  
-  if (!is.na(metadata[metadata[["variable"]] == variable, "derivation"])) {
-    write_out(paste0('* **Derivation:** ', metadata[metadata[["variable"]] == variable, "derivation"]))
+  if (!is.na(metadata[metadata[["variable"]] == variable, "description"])) {
+    write_out(paste0('* **Description:** ', metadata[metadata[["variable"]] == variable, "description"]))
   }
   
   if (!is.na(metadata[metadata[["variable"]] == variable, "comments"])) {
     write_out(paste0('* **Comments:** ', metadata[metadata[["variable"]] == variable, "comments"]))
   }
   
-  #chunk_wrapper(paste0('plot_distribution(x = \"', variable, '\")'))
+  write_out("\n")
+  
+  write_out(paste0('#### Distribution \n'))
+  
+  chunk_wrapper(
+    plot_distribution(variable = variable), 
+    options = c("echo = FALSE", "warning = FALSE", "message = FALSE", "fig.height = 2.5", "fig.width = 4.2", "fig.align = 'center'")
+  )
 
   chunk_wrapper(paste0('summarize_data(variable = \"', variable, '\")'))
   
-  if (length(unique(data[[variable]])) < 8 | is.factor(data[[variable]])) {
+  if (is.factor(data[[variable]]) | length(unique(data[[variable]])) < 10) {
     chunk_wrapper(paste0('summarize_levels(variable = \"', variable, '\")'))
   }
   
   if (is.numeric(data[[variable]]) | inherits(data[[variable]], "Date")) {
-    write_out(paste0('* **Lowest Unique Values:** ', lowest_values(data[[variable]])))
-    write_out(paste0('* **Highest Unique Values:** ', highest_values(data[[variable]])))
+    write_out(paste0('##### Epoch 1 \n'))
+    write_out(paste0('* **Lowest Unique Values:** ', lowest_values(data[data[["epoch"]] == 1, variable])))
+    write_out(paste0('* **Highest Unique Values:** ', highest_values(data[data[["epoch"]] == 1, variable])))
+    
+    write_out(paste0('##### Epoch 2 \n'))
+    write_out(paste0('* **Lowest Unique Values:** ', lowest_values(data[data[["epoch"]] == 2, variable])))
+    write_out(paste0('* **Highest Unique Values:** ', highest_values(data[data[["epoch"]] == 2, variable])))
+    
+    write_out(paste0('##### Epoch 3 \n'))
+    write_out(paste0('* **Lowest Unique Values:** ', lowest_values(data[data[["epoch"]] == 3, variable])))
+    write_out(paste0('* **Highest Unique Values:** ', highest_values(data[data[["epoch"]] == 3, variable])))
+    
+    write_out(paste0('##### Epoch 4 \n'))
+    write_out(paste0('* **Lowest Unique Values:** ', lowest_values(data[data[["epoch"]] == 4, variable])))
+    write_out(paste0('* **Highest Unique Values:** ', highest_values(data[data[["epoch"]] == 4, variable])))
   }
   
   if (!is.na(metadata[metadata[["variable"]] == variable, "figure_label"])) {
@@ -48,7 +61,7 @@ create_individual_entry <- function(data = merged.df, metadata = metadata.df, va
   }
   
   if (sum(is.na(data[[variable]])) > 0) {
-    write_out("\n#### Participants with missing values for this variable: \n")
+    write_out("\n#### IDs of Participants with Missing Values by Epoch: \n")
     write_out(paste0('* **Epoch 1:** ', print_missing(data = data[data[["epoch"]] == 1, ], variable = variable, id = "map.id")))
     write_out(paste0('* **Epoch 2:** ', print_missing(data = data[data[["epoch"]] == 2, ], variable = variable, id = "map.id")))
     write_out(paste0('* **Epoch 3:** ', print_missing(data = data[data[["epoch"]] == 3, ], variable = variable, id = "map.id")))
